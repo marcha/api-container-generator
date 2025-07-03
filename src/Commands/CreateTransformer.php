3de6a3098002ff7f25526637a5ddf61cb853da6d
marcha <marcic74@gmail.com>
2025-07-03 22:46:36 +0200

    ispravka sutba za resource i controlera

--- src/Commands/CreateTransformer.php
+++ src/Commands/CreateTransformer.php
@@ -8,79 +8,80 @@ use Illuminate\Support\Composer;
 use Marcha\Acg\Commands\Traits\ContainerMakeTrait;
 
 
-class CreateTransformer extends Command
+class CreateResource extends Command
 {
-    use ContainerMakeTrait;
+  use ContainerMakeTrait;
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'marcha:create-transformer 
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'marcha:create-resource
                             {container_name}
                             {--namespace=api : Container\'s namespace. Must be defined in composer.json}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create transformer in container.';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Create resource in container.';
 
 
-    /**
-     * Create a new command instance.
-     *
-     * @param Filesystem $files
-     */
-    public function __construct(Filesystem $files)
-    {
-        parent::__construct();
-        $this->files = $files;  
-        $this->composer = app()['composer'];   
+  /**
+   * Create a new command instance.
+   *
+   * @param Filesystem $files
+   */
+  public function __construct(Filesystem $files)
+  {
+    parent::__construct();
+    $this->files = $files;
+    $this->composer = app()['composer'];
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+    $containerName = $this->argument('container_name');
+
+    $this->setNamespace($this->option('namespace'));
+    $this->info('API path: ' . $this->namespacePath);
+    $this->containerPath = $this->getContainerPath($containerName);
+    $this->info('Container path: ' . $this->containerPath);
+
+    $this->table = $containerName;
+    $this->singularForm = $this->getTableFromContainer();
+
+    if (!empty($this->singularForm)) {
+      $this->makeResource();
+
+      $this->composer->dumpAutoloads();
+    } else {
+      $this->error('Model name is empty. Transformer not created!');
+    }
+  }
+
+  private function getTableFromContainer()
+  {
+
+    $model = '';
+
+    $path = $this->containerPath . 'Models' . DIRECTORY_SEPARATOR;
+    $files = array_diff(scandir($path), ['..', '.']);
+
+    if (count($files) == 1) {
+      $fileName = current($files);
+      $model = substr($fileName, 0, strrpos($fileName, '.'));
+    } else {
+      $model = $this->ask('Specify model/table name');
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $containerName = $this->argument('container_name');
-
-        $this->setNamespace($this->option('namespace'));
-        $this->info('API path: ' . $this->namespacePath);
-        $this->containerPath = $this->getContainerPath($containerName);
-        $this->info('Container path: ' . $this->containerPath);
-
-        $this->table = $containerName;
-        $this->singularForm = $this->getTableFromContainer();
-        
-        if (!empty($this->singularForm)) {
-            $this->makeTransformer();
-
-            $this->composer->dumpAutoloads();
-        } else {
-            $this->error('Model name is empty. Transformer not created!');
-        }
-    }
-
-    private function getTableFromContainer() {
-
-        $model = '';
-
-        $path = $this->containerPath . 'Models' . DIRECTORY_SEPARATOR;
-        $files = array_diff(scandir($path), ['..', '.']);
-
-        if (count($files) == 1) {
-            $fileName = current($files);
-            $model = substr($fileName, 0, strrpos($fileName, '.'));
-        } else {
-            $model = $this->ask('Specify model/table name');
-        }
-
-        return $model;
-    }
+    return $model;
+  }
 }
