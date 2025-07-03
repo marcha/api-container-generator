cde65469fad5e67af8222c01f7a808aaf937c771
marcha <marcic74@gmail.com>
2025-07-03 22:51:51 +0200

    izmenjne servicepprovider

--- src/AcgServiceProvider.php
+++ src/AcgServiceProvider.php
@@ -6,42 +6,41 @@ use Illuminate\Support\ServiceProvider;
 
 class AcgServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //$this->registerContainerCreator();
-    }
+  /**
+   * Register services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //$this->registerContainerCreator();
+  }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {        
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                Commands\CreateContainer::class,
-                Commands\CreateContainers::class,
-                Commands\CreateTransformer::class,
-            ]);
-        } 
+  /**
+   * Bootstrap services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    if ($this->app->runningInConsole()) {
+      $this->commands([
+        Commands\CreateContainer::class,
+        Commands\CreateContainers::class,
+        Commands\CreateResource::class,
+      ]);
     }
+  }
 
-    /**
-     * Register the make:migration generator.
-     */
-    private function registerContainerCreator()
-    {
-        $this->app->singleton('command.marcha.create-container', function ($app) {
-            return $app['Marcha\Acg\Commands\CreateContainer'];
-        });
-       
-        $this->commands('command.marcha.create-container');
-    }
+  /**
+   * Register the make:migration generator.
+   */
+  private function registerContainerCreator()
+  {
+    $this->app->singleton('command.marcha.create-container', function ($app) {
+      return $app['Marcha\Acg\Commands\CreateContainer'];
+    });
 
+    $this->commands('command.marcha.create-container');
+  }
 }
